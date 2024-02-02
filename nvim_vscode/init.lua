31abb730fc496a506e784863717b5d9c8fbe963a
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-02-02 14:33:15 -0500

    vscode neovim

--- nvim_vscode/init.lua
+++ nvim_vscode/init.lua
@@ -1,4 +1,5 @@
 
+local vscode = require('vscode-neovim')
 -- moves the current line to up and down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -9,8 +10,21 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<leader>sar", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = '[S]earch [a]ll current file and [R]eplace' });
 
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
 -- Window management
 -- vim.keymap.set("n", "<C-h>", "<C-w>h");
 -- vim.keymap.set("n", "<C-l>", "<C-w>l");
 -- vim.keymap.set("n", "<C-k>", "<C-w>k");
 -- vim.keymap.set("n", "<C-j>", "<C-w>j");
+
+-- buffer stuff
+vim.keymap.set("n", "<S-h>", ":bprevious<Enter>")
+vim.keymap.set("n", "<S-l>", ":bnext<Enter>")
