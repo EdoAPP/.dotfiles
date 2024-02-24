9eac92a09ea26f4fb2cd79e1d3ba3c3807864e26
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-02-23 21:19:47 -0500

    Replace custom config in favor of custom LazyVim config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+
+-- save file
+map({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+-- moves the current line to up and down
+map({ "v" }, "K", ":m '<-2<CR>gv=gv")
+map({ "v" }, "J", ":m '>+1<CR>gv=gv")
+
+-- windows
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "[W]indow [D]elete", remap = true })
+map("n", "<leader>ws", "<C-W>s", { desc = "Split window [S]ide", remap = true })
+map("n", "<leader>wv", "<C-W>v", { desc = "Split window [V]ertical", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
+
+map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  -- disable trouble
+  { "folke/trouble.nvim", enabled = false },
+  -- { "gbprod/yanky.nvim", enabled = false },
+  -- { "mfussenegger/nvim-dap", enabled = false },
+  -- { "rcarriga/nvim-dap-ui", enabled = false },
+}
