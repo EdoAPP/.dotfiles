41757f580dcfd4cc8034c47c0a09fc9ecde49494
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-03-28 17:56:17 -0400

    notes for future install.sh update

--- install.sh
+++ install.sh
@@ -2,18 +2,26 @@
 #
 dot=${HOME}/.config
 
-confirm () {
-    read -p "${1}? [y|n]" yn
-    echo
-    if [[ $yn =~ ^[yY]$ ]]; then
-        return 0
-    else
-        return 1
-    fi
+##need to install
+# homebrew
+# kitty
+# ripgrep (for telescope)
+# fd (for telescope)
+# lazygit
+# fish
+#
+#
+confirm() {
+	read -p "${1}? [y|n]" yn
+	echo
+	if [[ $yn =~ ^[yY]$ ]]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 # Zsh
 if confirm "fish"; then
 
 fi
-
