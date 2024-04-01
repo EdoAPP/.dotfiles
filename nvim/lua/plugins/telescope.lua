6a7f3cf31f846ef93c1e333888f72b65581cbaf1
Eduardo Pacheco <edo@Eduardos-MacBook-Air.local>
2024-04-01 16:41:59 -0400

    ignore .yarn folder from telescope

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  -- opts will be merged with the parent spec
+  opts = {
+    defaults = {
+      file_ignore_patterns = { ".git/", "node_modules", ".yarn/" },
+    },
+  },
+}
