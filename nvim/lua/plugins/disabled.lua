750e040a824a2e84b686138899179ce957ac2187
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-04-13 11:17:06 -0400

    reenable quickfix and trouble commands

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,9 +1,5 @@
 return {
   -- disable trouble
-  {
-    "folke/trouble.nvim",
-    enabled = false,
-  },
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,32 +14,6 @@ return {
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
-  {
-    "echasnovski/mini.bufremove",
-
-    keys = {
-      {
-        "<leader>x",
-        function()
-          local bd = require("mini.bufremove").delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = "Delete Buffer",
-      },
-      -- stylua: ignore
-      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-    },
-  },
   -- Finds and lists all of the TODO, HACK, BUG, etc comment
   -- in your project and loads them into a browsable list.
   {
