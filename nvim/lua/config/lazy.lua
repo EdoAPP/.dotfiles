b900e05b490d928305104f1fb42d0bd9f3c58e95
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-06-23 22:24:40 -0400

    Update: Make sure to update to nvim 0.10

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,3 +1,4 @@
+--NOTE: configuration lies in /.local/shared/nvim/lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
