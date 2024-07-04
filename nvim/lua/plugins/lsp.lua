bd4b1f4bf09e648502bf35e7c167d98e37bb66c5
Eduardo Pacheco <edo@Eduardos-MacBook-Air.local>
2024-07-04 10:04:33 -0400

    extend tailwind lsp config to support cva

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      if not opts.servers then
+        opts.servers = {}
+      end
+      opts.servers.tailwindcss = {
+        settings = {
+          tailwindCSS = {
+            experimental = {
+              classRegex = {
+                { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+}
