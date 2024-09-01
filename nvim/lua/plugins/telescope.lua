04777d2249e50e416c20f14a8014215dd1314347
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-08-31 23:29:04 -0400

    remove telescope custom config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,9 @@
 return {
-  "nvim-telescope/telescope.nvim",
+  -- "nvim-telescope/telescope.nvim",
   -- opts will be merged with the parent spec
-  opts = {
-    defaults = {
-      file_ignore_patterns = { ".git/", "node_modules", ".yarn/" },
-    },
-  },
-  keys = {
-    { "<leader><space>", require("telescope.builtin").find_files, desc = "Find Files (Root Dir)" },
-  },
+  -- opts = {
+  --   defaults = {
+  --     file_ignore_patterns = { ".yarn/" },
+  --   },
+  -- },
 }
