583e7d2f8caeed2696d34c06f131a92f1efcaeee
Edo <eduardoapp.97@gmail.com>
2024-12-19 15:41:44 -0500

    update to lazyvim 14

--- nvim/lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- "nvim-telescope/telescope.nvim",
-  -- opts will be merged with the parent spec
-  -- opts = {
-  --   defaults = {
-  --     file_ignore_patterns = { ".yarn/" },
-  --   },
-  -- },
-}
