73384110f7f41d6c88af287cac3568934f86fe94
Edo <eduardoapp.97@gmail.com>
2025-03-19 13:55:08 -0400

    Remove lua line & zed update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,18 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldmethod = "expr"
 
 vim.opt.scrolloff = 8
+vim.opt.ls = 0
+
+-- •	⁠shortmess controls how messages are displayed. The options added above do the following:
+-- •	⁠F: Don't show the "found" message when searching for a file.
+-- •	⁠I: Don't show the intro message when starting Neovim.
+-- •	⁠W: Don't show the "written" message when saving a file.
+-- •	⁠c: Suppresses the "changed" message when a file is modified.
+-- •	⁠s: Suppresses the "status" messages.
+vim.opt.shortmess:append("F")
+vim.opt.shortmess:append("I")
+vim.opt.shortmess:append("W")
+vim.opt.shortmess:append("c")
+vim.opt.shortmess:append("s")
+vim.opt.showmode = false -- Don't show mode in the status line
+vim.opt.showcmd = false -- Don't show command in the status line
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   -- disable trouble
   { "folke/noice.nvim", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
   -- { "mfussenegger/nvim-dap", enabled = false },
   -- { "rcarriga/nvim-dap-ui", enabled = false },
 }
