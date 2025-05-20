f6df357256d5cd274d8bb53c9795be77179fd3ed
Edo <eduardoapp.97@gmail.com>
2025-05-20 15:06:50 -0400

    add docker to path

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,10 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+set --export DOCKER_INSTALL "/Applications/Docker.app/Contents/Resources/"
+set --export PATH $DOCKER_INSTALL/bin $PATH
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/edo/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/edo/Downloads/google-cloud-sdk/path.fish.inc'; end
+if [ -f '/Users/edo/Downloads/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/edo/Downloads/google-cloud-sdk/path.fish.inc'
+end
